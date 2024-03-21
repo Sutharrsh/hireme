63d027be10a2cbfd82966da45f866795670bab9f
sutharrsh <sutharharsh16gmail.com>
2024-03-21 23:17:37 +0530

    Admin Added

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -24,8 +24,8 @@ class UserController
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'harsh.suthar@groovyweb.co';
-            $mail->Password = 'yuezqehgzsowsswf';
+            $mail->Username = '';
+            $mail->Password = '';
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
 
